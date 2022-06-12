559b0c6a71757214d8d82e6f0fc8f7ae7d9ea661
krisstish <aks.kristina1@gmail.com>
2022-06-12 23:19:36 +0200

    mysql.php

---
+++ mysql.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "ad.php";
+
+class mysql
+{
+    private mysqli $mysqli;
+
+    public function __construct() 
+    {
+        $this->mysqli = new mysqli('f', '=', 'arg', 'z');
+        if (mysqli_connect_errno())
+            throw new mysqli_sql_exception();
+    }
+
+    public function saveAd(Ad $ad) 
+    {
+        $statement = $this->mysqli->prepare("INSERT INTO ad (email, title, description, category) VALUES (?, ?, ?, ?)");
+        $email = $ad->getEmail();
+        $title = $ad->getTitle();
+        $description = $ad->getDescription();
+        $category = $ad->getCategory();
+
+        if($statement->bind_param("ssss", $email, $title, $category, $description)===false || $statement->execute()===false || $statement->close()===false)
+            throw new mysqli_sql_exception();
+    }
+
+    public function getAds() 
+    {
+        $adsArr = [];
+        if ($result = $this->mysqli->query('SELECT * FROM ad')) 
+        {
+            while($row = $result->fetch_assoc())
+                $adsArr[] = new ad($row["email"], $row["category"], $row["title"], 
+                    $row["description"]);
+            $result->close();
+        }
+        return $adsArr;
+    }
+
+    public function getCategories() 
+    {
+        $categories = [];
+        if ($result = $this->mysqli->query('SELECT category FROM table_name'))
+        {
+            while($row = $result->fetch_assoc())
+                $categories[] = $row["category"];
+            $result->close();
+        }
+        return $categories;
+    }
+}  
